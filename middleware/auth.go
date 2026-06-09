fcbb606b0145719b4542193c1aba92d616054511
rafael <rafaelrangel@singularpharma.com.br>
2026-06-09 16:03:31 -0300

    fix: fix some speling errors in auth.go

--- middleware/auth.go
+++ middleware/auth.go
@@ -15,14 +15,14 @@ func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
     authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			c.JSON(http.StatusUnautorized, gin.H{"error": "Authorization Header absent"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Header absent"})
 			c.Abort()
 			return
 		}
 
     parts := strings.SplitN(authHeader, " ", 2)
     if len(parts) != 2 || parts[0] != "Bearer" {
-      c.JSON(http.StatusUnautorized, gin.H{"error": "Invalid token format. Use 'Bearer <token>'"})
+      c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format. Use 'Bearer <token>'"})
       c.Abort()
       return
     }
@@ -30,23 +30,23 @@ func AuthMiddleware() gin.HandlerFunc {
     tokenString := parts[1]
 
     token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-      if _, ok := token.Method.(*jwt.SigninMethodHMAC); !ok {
+      if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
         return nil, fmt.Errorf("assign method waited: %v", token.Header["alg"])
       }
       return []byte(config.GlobalConfig.JWTSecret), nil
     })
 
     if err != nil || !token.Valid {
-      c.JSON(http.StatusUnautorized, gin.H{"error": fmt.Sprintf("Token inválido: %v", err)})
+      c.JSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("Token inválido: %v", err)})
       c.Abort()
       return
     }
 
     claims, ok := token.Claims.(jwt.MapClaims)
     if !ok {
-      c.JSON(http.StatusUnautorized, gin.H{"error", "Failed to read data (claims) from token"})
+      c.JSON(http.StatusUnauthorized, gin.H{"error": "Failed to read data (claims) from token"})
       c.Abort()
-      return4
+      return
     }
 
     tenantID, exists := claims["tenant_id"]
@@ -55,7 +55,7 @@ func AuthMiddleware() gin.HandlerFunc {
     }
 
     if !exists {
-      c.JSON(http.StatusUnautorized, gin.H{"error": "tenant_id is missing from token claims"})
+      c.JSON(http.StatusUnauthorized, gin.H{"error": "tenant_id is missing from token claims"})
       c.Abort()
       return
     }
