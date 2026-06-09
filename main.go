79c84fe14d3c2d3bde68495f4f382b412b314c82
rafael <rafaelrangel@singularpharma.com.br>
2026-06-09 15:59:59 -0300

    feat: create the main.go to export the endpoints and handleIngestion method

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"sensor-ingestion-go/config"
+	"sensor-ingestion-go/middleware"
+	"sensor-ingestion-go/rabbitmq"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SensorPayload struct {
+	MacAddress		string		`json:"macAddress" binding:"required"`
+	Temperature		float64 	`json:"temperature" binding:"required"`
+	Humidity		float64		`json:"humidity" binding:"required"`
+	Timestamp		string		`json"timestamp" binding:"required"`
+}
+
+type QueueMessage struct {
+	SensorPayload
+	TenantID strin `json:"tenantId"`
+}
+
+func main() {
+	log.Println("[App] Starting Sensor Ingestion...")
+
+	config.LoadConfig()
+
+	rabbitmq.InitRabbitMQ()
+
+	defer rabbitmq.GlobalPublisher.Close()
+
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOk, gin.H{
+			"status": "healthy",
+			"service": "sensor-ingestion-go",
+		})
+	})
+
+  r.POST("/ingest", middleware.AuthMiddleware(), handleIngestion)
+
+  port := config.GlobalConfig.Port
+  log.Printf("[APP] Server boot at %s", port)
+
+  if err := r.Run(":" + port); err != nil {
+    log.Fatalf("CRITICAL ERROR: Failed to boot HTTP server: %v", err)
+  }
+}
+
+func handleIngestion(c *gin.Context) {
+  var payload SensorPayload
+
+  if err := c.ShouldBindJSON(&payload); err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{
+      "error":    "Invalid Payload or missing values",
+      "details":  err.Error(),
+    })
+    return
+  }
+
+  tenantIDRaw, exists := c.Get("tenant_id")
+  if !exists {
+    c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Error to get tenant_id"})
+    return
+  }
+  tenantID := tenantIDRaw.(string)
+
+  message := QueueMEssage{
+    SensorPayload: payload,
+    TenantID:      tenantID,
+  }
+
+  messageBytes, err := json.Marshal(message)
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to serialize message"})
+    return
+  }
+
+  if err =: rabbitmq.GlobalPublisher.Publish(messageBytes); err != nil {
+    log.Printf("[ERRO] Failed to public message in queue: %v", err)
+    c.JSON(http.StatusServiceUnavailable, gin.H{
+      "error": "Message service unavailable temporary",
+    })
+    return
+  }
+
+  c.JSON(http.StatusAccepted, gin.H{
+    "message": "Sensor read recieved with success and send to process",
+    "tenantId": tenantID,
+  })
+}
